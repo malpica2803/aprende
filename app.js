9b1270eba3f489b95858a62d5e75055414611614
malpica2803 <simbron_malpica@hotmail.com>
2021-03-22 10:44:35 -0600

    muestra funciones dentro de funciones y ventanas emergentes

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console,console.log("holaaaa");
+'use stric';
+
+/*
+let n1 = prompt('INTRODUCE NOMBRE 1','ADOLFO');
+let n2 = prompt('INTRODUCE NOMBRE 2','ALEX');
+
+function mostrarConsola(n1, n2){
+    console.log(n1 + ' '+ n2);
+}
+
+function mostrarDocumento(n1, n2){
+    document.write(n1 + ' '+ n2);
+}
+
+function mostrar(n1, n2, n3 = false){
+    if(n3 == false){
+        mostrarConsola(n1,n2);
+    }else{
+        mostrarDocumento(n1,n2);
+    }
+
+}
+
+
+mostrar(n1,n2,false);
+*/
+
+
+alert('saludos');
+
+
+var res = confirm('Estas Seguro?');
+
+var otro = prompt('estas dicendome tu nombre?');
+
+
+
+document.write (`BIENVENIDO ${ otro} te EStaba esperando`);
