d3c6a50ebc5ca0c1e6f252de752b03bdb736a4f1
malpica2803 <simbron_malpica@hotmail.com>
2021-03-22 13:03:45 -0600

    ahora plantilla de funciones del DOM

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ function mostrar(n1, n2, n3 = false){
 
 
 mostrar(n1,n2,false);
-*/
 
 
+// alertas en ventanas emergentes
 alert('saludos');
 
 
@@ -36,3 +36,40 @@ var otro = prompt('estas dicendome tu nombre?');
 
 
 document.write (`BIENVENIDO ${ otro} te EStaba esperando`);
+
+
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+    var guess;
+
+  
+	return -1;
+};
+
+*/
+
+// LLAMA ALA VARIABLE DIV1 Y LINKEA LA ID DESDE HTML EN EL TAG DIV- LLAMADO DIV1
+var div1 = document.getElementById('div1');
+div1.innerHTML = ('TEXTO MODIFICADO DESDE EL JS');
+div1.style.fontSize = '25px';
+
+console.log(div1);
+
+var parrafo1 = document.getElementsByClassName('parrafo1');
+parrafo1[2].innerHTML = ('PARRAFO CAMBIADO');
+parrafo1[2].style.fontFamily = 'arial';
+console.log(parrafo1);
+
+var boton = document.querySelector('#boton');
+var div2= document.querySelector('#div2');
+/*
+boton.addEventListener('click',function(){
+    div2.innerHTML = ('<br>Has entrado ala Sesion<br>');
+});*/
+
+function pulsar(){
+    div2.innerHTML = ('<br>Has entrado ala Sesion<br>');
+}
+
+
